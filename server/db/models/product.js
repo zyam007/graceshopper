39bb3c7e0db71f37ed8670e42e29cfe50422db0c
Aarluk <provencenewyork@gmail.com>
2020-11-11 16:54:06 -0500

    FEATURE: created test for category model

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -26,7 +26,7 @@ const Product = db.define('product', {
       min: 0.0
     }
   },
-  imageURL: {
+  imageUrl: {
     type: Sequelize.ARRAY(Sequelize.STRING),
     defaultValue: [
       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRR_X_QkxnyC_FJtYFgqyCWT6s7dnQyNQ9ifw&usqp=CAU'
