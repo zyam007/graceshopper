2c18f9822adbdfdf051b4e3771821d86236de6ce
Kadecahe <kadecahe@gmail.com>
2020-11-11 12:57:53 -0500

    Feat: Updates test files for User.specs

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -1,7 +1,7 @@
 /* global describe beforeEach it */
 
 const {expect} = require('chai')
-const db = require('../index')
+const {db} = require('../index')
 const User = db.model('user')
 
 describe('User model', () => {
@@ -15,13 +15,15 @@ describe('User model', () => {
 
       beforeEach(async () => {
         cody = await User.create({
+          firstName: 'Cody',
+          lastName: 'Pup',
           email: 'cody@puppybook.com',
-          password: 'bones'
+          password: 'bones!'
         })
       })
 
       it('returns true if the password is correct', () => {
-        expect(cody.correctPassword('bones')).to.be.equal(true)
+        expect(cody.correctPassword('bones!')).to.be.equal(true)
       })
 
       it('returns false if the password is incorrect', () => {
@@ -29,4 +31,48 @@ describe('User model', () => {
       })
     }) // end describe('correctPassword')
   }) // end describe('instanceMethods')
+  describe('User has first name and last name', () => {
+    let cody
+
+    beforeEach(async () => {
+      cody = await User.create({
+        firstName: 'Cody',
+        lastName: 'Pup',
+        email: 'cody@puppybook.com',
+        password: 'bones!'
+      })
+    })
+
+    it('User has a valid first name', () => {
+      expect(cody.firstName).not.to.equal(null)
+      expect(typeof cody.firstName).to.be.a('string')
+      expect(cody.firstName).to.equal('Cody')
+    })
+
+    it('User has a valid last name', () => {
+      expect(cody.lastName).not.to.equal(null)
+      expect(typeof cody.lastName).to.be.a('string')
+      expect(cody.lastName).to.equal('Pup')
+    })
+  }) //end describe user has first name and last name
+
+  describe('User has an email', () => {
+    let cody
+
+    beforeEach(async () => {
+      cody = await User.create({
+        firstName: 'Cody',
+        lastName: 'Pup',
+        email: 'cody@puppybook.com',
+        password: 'bones!'
+      })
+    })
+
+    it('User email has the correct format', () => {
+      let result = cody.email.includes('@')
+      expect(result).to.be.a('boolean')
+      expect(result).to.equal(true)
+      expect(cody.email).to.equal('cody@puppybook.com')
+    })
+  }) //end email tests
 }) // end describe('User model')
