bdb07720f2d413e3677064c279912922ace3c7af
Yanna Sk <zyam007@gmail.com>
2020-11-11 18:02:10 -0500

    renamed home to all products

--- client/app.js
+++ client/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
-//import ConnectedHome from './components/Home'
+import ConnectedAllProducts from './components/AllProducts'
 
 const App = () => {
   return (
