9fbb355cb102a04a67f1ad8b7b3b62443aae19da
Kadecahe <kadecahe@gmail.com>
2020-11-11 20:43:10 -0500

    Feat: added individual user and user attributes

--- server/api/users.js
+++ server/api/users.js
@@ -8,9 +8,20 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
+      attributes: ['firstName', 'lastName', 'id', 'email']
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+
+    if (!user) return res.sendStatus(404)
+    res.json(user)
   } catch (err) {
     next(err)
   }
