89d645226dd6afa0f021af0f53ce5130b1d03596
Aarluk <provencenewyork@gmail.com>
2020-11-12 13:17:21 -0500

    FEAT: added a footer, still working on styling it

---
+++ client/components/footer.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import styled from 'styled-components'
+
+const Footer = () => {
+  return (
+    <FooterContainer className="main-footer">
+      <div className="footer-middle">
+        <div className="Container">
+          <div className="row">
+            {/* Column 1 */}
+            <div className="col-md-3 col-sm-6">
+              <h5>Contact Us</h5>
+              <ul className="list-unstyled">
+                <li>Email: allcaps@gh.com</li>
+                <li>Tel: 123.456.7890</li>
+                <li>GitHub</li>
+              </ul>
+            </div>
+            {/* Column 2 */}
+            <div className="col-md-3 col-sm-6">
+              <h5>Team Members</h5>
+              <ul className="list-unstyled">
+                <li>Yanna Skorokhodova</li>
+                <li>Kade Cahe</li>
+                <li>Virginie Bonnet</li>
+              </ul>
+            </div>
+          </div>
+          {/* Footer Bottom: copyrights */}
+          <div className="footer-bottom">
+            <p className="text-xs-center">
+              &copy;{new Date().getFullYear()} All-Caps App - All Rights
+              Reserved
+            </p>
+          </div>
+        </div>
+      </div>
+    </FooterContainer>
+  )
+}
+
+export default Footer
+
+const FooterContainer = styled.footer`
+  .footer-middle {
+    background: var(--mainDark);
+    padding-top: 1rem;
+    padding-left: 30rem;
+    color: var(--mainLightGrey);
+    font-family: sans-serif;
+  }
+  .footer-bottom {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+`
