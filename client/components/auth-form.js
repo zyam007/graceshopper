f7f68fbf351bad37f125ba36054e74b37684471f
Yanna Sk <zyam007@gmail.com>
2020-11-16 15:09:17 -0500

    still working on form

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,74 +2,114 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
-
+import {Form, Button, Row, Col} from 'react-bootstrap'
 /**
  * COMPONENT
  */
 const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
-  console.log('display name', displayName)
+  const [validated, setValidated] = useState(false)
+
   return (
-    <div className="signup-container">
-      <form onSubmit={handleSubmit} name={name}>
-        {displayName === 'Sign Up' && (
-          <div>
-            <div className="form-group">
-              <label htmlFor="firstName">
-                <small>First Name</small>
-              </label>
-              <input
-                name="firstName"
-                type="text"
-                className="form-control"
-                placeholder="Enter first name"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="lastName">
-                <small>Last Name</small>
-              </label>
-              <input
-                name="lastName"
-                type="text"
-                className="form-control"
-                placeholder="Enter last name"
-              />
-            </div>
-          </div>
-        )}
-        <div className="form-group">
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input
-            name="email"
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input
-            name="password"
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form.Row>
+        <Form.Group controlId="formGridFirstName">
+          <Form.Label htmlFor="firstName">First Name</Form.Label>
+          <Form.Control required type="text" placeholder="First Name" />
+          <Form.Control.Feedback>Looks Good!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group controlId="formGridLastName">
+          <Form.Label htmlFor="lastName">Last Name</Form.Label>
+          <Form.Control required type="text" placeholder="Last name" />
+          <Form.Control.Feedback>LooksGood!</Form.Control.Feedback>
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group controlId="formGridEmail">
+          <Form.Label htmlFor="email">Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group controlId="formGridPassword">
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
             type="password"
-            className="form-control"
-            placeholder="Enter password"
+            placeholder="Password"
+            aria-describedby="passwordHelpMsg"
           />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      <div className="google-signup">
-        <a href="/auth/google">{displayName} with Google</a>
-      </div>
-    </div>
+          <Form.Text id="passwordHelpMsg" muted>
+            Must be 6-20 characters long.
+          </Form.Text>
+        </Form.Group>
+      </Form.Row>
+
+      <Button type="submit" variant="secondary">
+        Submit
+      </Button>
+    </Form>
   )
+  // return (
+  //   <div className="signup-container">
+  //     <form onSubmit={handleSubmit} name={name}>
+  //       {displayName === 'Sign Up' && (
+  //         <div>
+  //           <div className="form-group">
+  //             <label htmlFor="firstName">
+  //               <small>First Name</small>
+  //             </label>
+  //             <input
+  //               name="firstName"
+  //               type="text"
+  //               className="form-control"
+  //               placeholder="Enter first name"
+  //             />
+  //           </div>
+  //           <div className="form-group">
+  //             <label htmlFor="lastName">
+  //               <small>Last Name</small>
+  //             </label>
+  //             <input
+  //               name="lastName"
+  //               type="text"
+  //               className="form-control"
+  //               placeholder="Enter last name"
+  //             />
+  //           </div>
+  //         </div>
+  //       )}
+  //       <div className="form-group">
+  //         <label htmlFor="email">
+  //           <small>Email</small>
+  //         </label>
+  //         <input
+  //           name="email"
+  //           type="email"
+  //           className="form-control"
+  //           placeholder="Enter email"
+  //         />
+  //       </div>
+  //       <div className="form-group">
+  //         <label htmlFor="password">
+  //           <small>Password</small>
+  //         </label>
+  //         <input
+  //           name="password"
+  //           type="password"
+  //           className="form-control"
+  //           placeholder="Enter password"
+  //         />
+  //       </div>
+  //       <div>
+  //         <button type="submit">{displayName}</button>
+  //       </div>
+  //       {error && error.response && <div> {error.response.data} </div>}
+  //     </form>
+  //     <div className="google-signup">
+  //       <a href="/auth/google">{displayName} with Google</a>
+  //     </div>
+  //   </div>
+  // )
 }
 
 /**
@@ -98,8 +138,13 @@ const mapSignup = state => {
 const mapDispatch = dispatch => {
   return {
     handleSubmit(evt) {
+      const form = evt.currentTarget
+      if (form.checkvalidity() === false) {
+        evt.preventDefault()
+        evt.stopPropagation()
+      }
+      setValidated(true)
       evt.preventDefault()
-      console.log('form name: ', evt.target.name)
       const formName = evt.target.name
       const email = evt.target.email.value
       const password = evt.target.password.value
